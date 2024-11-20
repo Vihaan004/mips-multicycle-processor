69594a2ccfbfe06ad8d85f0575bbb7ccd9dd6e22
Vihaan Patel <vihaan004@gmail.com>
2024-11-19 22:06:29 -0700

    update

--- MULT16.asm
+++ MULT16.asm
@@ -1,21 +1,21 @@
-    andi    $0, $0, 0           # $0 = 0
-    addi    $1, $0, 6           # $1 = 6
-    ori     $2, $0, 18          # $2 = 18
-    add     $3, $1, $2          # $3 = $1 + $2 = 24
-    sub     $4, $2, $1          # $4 = $2 - $1 = 12
-    and     $5, $1, $2          # $5 = $1 AND $2 = 2
-    or      $6, $1, $2          # $6 = $1 OR $2 = 22
-    slt     $7, $1, $2          # $7 = 1 if $1 < $2, else 0
-    sll     $8, $2, 4           # $8 = $2 << 4 = 18 * 16 = 288
-    srl     $9, $1, 1           # $9 = $1 >> 1 = 6 / 2 = 3
-    beq     $1, $2, 1           # Branch if $1 == $2 (will not branch)
-    lw      $10, 4($0)          # Load word from memory address ($0 + 4) into $10
-    bne     $1, $2, 1           # Branch if $1 != $2 (will branch to PC + 1)
-    andi    $1, $1, 0           # $1 = 0 (skipped if bne worked correctly)
-    j       16                  # Jump to address 16
-    andi    $2, $2, 0           # $2 = 0 (skipped if j worked correctly)
-    jr      $2                  # Jump to address in $2
-    andi    $3, $3, 0           # $3 = 0 (skipped if jr worked correctly)
+    andi    $0, $0, 0 
+    addi    $1, $0, 6 
+    ori     $2, $0, 18
+    add     $3, $1, $2
+    sub     $4, $2, $1
+    and     $5, $1, $2
+    or      $6, $1, $2
+    slt     $7, $1, $2
+    sll     $8, $2, 4 
+    srl     $9, $1, 1 
+    beq     $1, $2, 1 
+    lw      $10, 4($0)
+    bne     $1, $2, 1 
+    andi    $1, $1, 0 
+    j       16        
+    andi    $2, $2, 0 
+    jr      $2        
+    andi    $3, $3, 0 
     mult16  $11, $1, $3         # $11 = $1 * $9 = 6 * 3 = 18
     mult16  $12, $8, $8         # $12 = $8 * $8 = 288 * 288 = 82944
     sw      $1, 64($0)
--- mips_multicycle.srcs/sim_1/new/led_tb.v
+++ mips_multicycle.srcs/sim_1/new/led_tb.v
@@ -3,17 +3,10 @@ module LED_Testbench ();
     reg CLK;
     reg RST;
     reg HALT;
-    // wire CS;
-    // wire WE;
-//    wire [31:0] Mem_Bus;
-//    wire [6:0] Address;
 
     wire [7:0] LED;
     wire [31: 0] Reg1;
     
-    // integer i;
-    // parameter N = 12;
-    // reg[31:0] expected[N:1];
     
     initial
     begin
@@ -26,24 +19,6 @@ module LED_Testbench ();
     
 	//This will need to change when you add more ports to the processor.
     Complete_MIPS uProc_Inst(CLK, RST, HALT, LED); 
-    
-    
-    // initial begin
-	// 	//Will need to change this as well depending upon the instructions you have in your instruction file
-    //     expected[1] = 32'h00000006; //
-    //     expected[2] = 32'h00000012; //
-    //     expected[3] = 32'h00000018; // 
-    //     expected[4] = 32'h0000000C; // 
-    //     expected[5] = 32'h00000002; // 
-    //     expected[6] = 32'h00000016; //
-    //     expected[7] = 32'h00000001; // 
-    //     expected[8] = 32'h00000120; //
-    //     expected[9] = 32'h00000003; // 
-    //     expected[10] = 32'h00412022; //
-    //     expected[11] = 32'h00000090; // 
-    //     expected[12] = 32'h00014400; // 
-    //     CLK = 0;
-    // end
     
     
     always
@@ -79,22 +54,6 @@ module LED_Testbench ();
         
         $stop;
 
-
-
-        // you can add in a 'Halt' signal here as well to test Halt operation
-        // you will be verifying your program operation using the
-        // waveform viewer and/or self-checking operations
-        // for(i = 1; i <= N; i = i+1) 
-        // begin
-        //     @(posedge uProc_Inst.WE); // When a store word is executed
-        //     @(negedge CLK);
-        //     if (Mem_Bus != expected[i])
-        //     begin
-        //         $display("Output mismatch: got %d, expect %d", Mem_Bus, expected[i]);
-        //     end
-        // end
-        // $display("TEST COMPLETE");
-//        $stop;
     end
     
 endmodule
